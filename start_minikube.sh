1c42faa699dbd72470c2e17f9317e15add8ae893
cmrfrd <iambubbi@f0rge.anvil>
2019-01-28 01:38:09 -0500

    Move minikube startup scripts to root dir of repo

--- start_minikube.sh
+++ start_minikube.sh
@@ -1,5 +1,9 @@
+## Create minikube profile  
+## for kubeconfig isolation
 minikube profile self2face
 
+## Start minikube without
+## gpu access under profile
 minikube start \
 	 --memory=10192 \
 	 --cpus=8 \
@@ -7,6 +11,6 @@ minikube start \
 	 --insecure-registry localhost:5000 \
 	 --profile serverless_datascience
 
+## Mount current working directory                       
+## into minikube for pod access
 minikube mount $(pwd):/mnt &
-
-eval $(minikube docker-env)
--- start_minikube_gpu.sh
+++ start_minikube_gpu.sh
@@ -1,17 +1,24 @@
+## Create minikube profile
+## for kubeconfig isolation
 minikube profile self2face
 
+## start minikube with gpu
+## and profile
 minikube start \
 	 --memory=10192 \
 	 --cpus=8 \
-	 --disk-size=30GB \
+	 --disk-size=42GB \
 	 --vm-driver=kvm2 \
 	 --gpu \
 	 --insecure-registry localhost:5000 \
 	 --profile self2face
 
+## Add nvidia plugins
+## for gpu access
 minikube addons enable nvidia-gpu-device-plugin
 minikube addons enable nvidia-driver-installer
 
+## Mount current working directory
+## into minikube for pod access
 minikube mount $(pwd):/mnt &
 
-eval $(minikube docker-env)
