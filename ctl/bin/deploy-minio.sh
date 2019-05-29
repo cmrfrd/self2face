d733f7703b8e205073ccaac4d7e42ed02f0893e0
cmrfrd <alexanderjcomerford@gmail.com>
2019-05-29 00:49:40 -0400

    Initial update minio, new filestructure, helm

---
+++ ctl/bin/deploy-minio.sh
@@ -0,0 +1,1 @@
+helm install stable/minio -n minio --set "accessKey=self2face,secretKey=self2face"
--- ctl/bin/forward_ports.sh
+++ ctl/bin/forward_ports.sh
@@ -1,2 +1,3 @@
 sh -c "(kubectl port-forward -n kubeflow svc/ambassador 7222:80 &
-        kubectl port-forward -n kube-system svc/kubernetes-dashboard 7333:80)"
+        kubectl port-forward -n kube-system svc/kubernetes-dashboard 7333:80 &
+        kubectl port-forward -n default svc/minio 7444:9000)"
